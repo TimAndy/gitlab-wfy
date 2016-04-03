87c16435546ae6c8c4128f9d63b5435a1165769c
TimAndy <xuchonglei@126.com>
2016-04-03 09:21:47 +0800

    时间描述汉化调整

--- app/assets/javascripts/lib/jquery.timeago.js
+++ app/assets/javascripts/lib/jquery.timeago.js
@@ -43,20 +43,20 @@
       strings: {
         prefixAgo: null,
         prefixFromNow: null,
-        suffixAgo: "之前",
-        suffixFromNow: "之后",
+        suffixAgo: "前",
+        suffixFromNow: "后",
         seconds: "不到 1 分钟",
-        minute: "大约 1 分钟",
+        minute: "约 1 分钟",
         minutes: " %d 分钟",
-        hour: "大约 1 小时",
-        hours: "大约 %d 小时",
+        hour: "约 1 小时",
+        hours: "约 %d 小时",
         day: " 1 天",
         days: " %d 天",
-        month: "大约 1 个月",
+        month: "约 1 个月",
         months: " %d 个月",
-        year: "大约 1 年",
+        year: "约 1 年",
         years: " %d 年",
-        wordSeparator: " ",
+        wordSeparator: "",
         numbers: []
       }
     },
@@ -96,7 +96,7 @@
         substitute($l.years, Math.round(years));
 
       var separator = $l.wordSeparator || "";
-      if ($l.wordSeparator === undefined) { separator = " "; }
+      if ($l.wordSeparator === undefined) { separator = ""; }
       return $.trim([prefix, words, suffix].join(separator));
     },
     parse: function(iso8601) {
